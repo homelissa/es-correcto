886888b7b48efbe3717ec25e8f096438f07f4833
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-06-25 14:35:15 -0700

    Frontend auth attempt

---
+++ frontend/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { register, login } from './util/session_api_utils';
+
+// import { Router, browserHistory } from 'react-router';
+// import reduxThunk from 'redux-thunk';
+// import store
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  window.register = register;
+  window.login = login;
+
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>hello</h1>, root);
+});
