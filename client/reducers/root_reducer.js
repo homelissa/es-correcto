2c1493c7d304ae5b9e5deb2a5ced163ac6440b55
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-06-29 08:31:05 -0700

    Add plan reducer to root reducer

--- client/reducers/root_reducer.js
+++ client/reducers/root_reducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import authReducer from './auth_reducer';
 import userReducer from './user_reducer';
 import productReducer from './product_reducer';
+import planReducer from './plan_reducer';
 
 const rootReducer = combineReducers({
 
   auth: authReducer,
   user: userReducer,
   products: productReducer,
+  plans: planReducer
 });
 
 export default rootReducer;
