cab4db74238a2ccc61361f37f5ad1811b301e0e2
sandzu <a.schaefer1995@gmail.com>
2018-06-29 16:39:22 -0700

    add frontend access to product img_url

--- client/components/products/productIndex.jsx
+++ client/components/products/productIndex.jsx
@@ -15,19 +15,25 @@ class ProductIndex extends React.Component {
               {product.name}
             </h4>
           <div>
-            {product.plans.map(plan =>
-              <ul>
-                <li>
-                  Cost: {plan.cost}
-                </li>
-                <li>
-                  Payment Frequency: {plan.paymentFrequency}
-                </li>
-                <li>
-                  Contract Length: {plan.contractLength}
-                </li>
-              </ul>
-            )}
+            <ul id="index-list">
+              {product.plans.map(plan =>
+                <li id="index-item">
+                <ul>
+                  <li>
+                    Cost: {plan.cost}
+                  </li>
+                  <li>
+                    Payment Frequency: {plan.paymentFrequency}
+                  </li>
+                  <li>
+                    Contract Length: {plan.contractLength}
+                  </li>
+
+                  <img src={`${product.img_url}`} alt="Italian Trulli" /> 
+                </ul>
+              </li>
+              )}
+            </ul>
           </div>
         </div>
         )}
